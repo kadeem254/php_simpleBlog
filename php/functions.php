919573207c74d324e86eb6f6bfd7f0ee7a4053b5
KDeeM <kadeemalexander254@gmail.com>
2022-08-18 09:49:56 +0300

    Relocated Fetch Posts Function
    Moved the FetchPosts from the ./admin.php page to the ./php/functions.php page as it is a reusabble code block and would be easier to acces from the shared module.

--- php/functions.php
+++ php/functions.php
@@ -17,4 +17,59 @@
 
     return $connection;
   }
+
+  /**Fetches Posts based on a given query */
+  function FetchPosts( $query = "SELECT * FROM blog" ){
+    // establish a connection to the database
+    try{
+      $conn = CreateDatabaseConnection();
+    } catch ( Exception $err ){
+      throw new Exception( $err->getMessage() );
+      return false;
+    }
+
+    // get the posts currently in the database
+    try{
+      $results = $conn->query( $query );
+      
+      // if no results return Null
+      if( $results->num_rows <= 0 ){
+        $conn->close();
+        return NULL;
+      }
+
+    }
+    catch( Exception $err ){
+      throw new Exception( $err->getMessage() );
+      $conn->close();
+      return false;
+    }
+
+    // Pack the results in and object
+    try{
+      $blog_list = array();
+
+      while( $row = $result->fetch_assoc() ){
+        $entry = array(
+          "blog_id"=>$row["blog_id"],
+          "title"=>$row["title"],
+          "active"=>$row["active"],
+          "created_on"=>$row["created_on"],
+          "last_update"=>$row["last_update"],
+        );
+
+        array_push( $blog_list, $entry );
+
+        // close connection when done
+        $conn->close();
+        return $blog_list;
+      }
+    }
+    catch( Exception $err ){
+      throw new Exception( $err->getMessage() );
+      $conn->close();
+      return false;
+    }
+
+  }
 ?>
